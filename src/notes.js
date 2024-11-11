6f69f2a8116d7b2af3f35f73670baa7990fba4d5
sidhu <khushdeep>
2024-11-11 11:06:09 -0500

    update for deployment

--- src/notes.js
+++ src/notes.js
@@ -1,119 +1,113 @@
-import uuidv4 from 'uuid/v4'
-import moment from 'moment'
+import { v4 as uuidv4 } from 'uuid';
+import moment from 'moment';
 
-let notes = []
+let notes = [];
 
 // Check for existing saved data in local storage
 const loadNotes = () => {
+  const notesJSON = localStorage.getItem('notes');
 
-    const notesJSON = localStorage.getItem ( 'notes' )
-
-    try {
-       return  notesJSON ? JSON.parse ( notesJSON  ) : []
-    } catch ( e ) {
-        return []
-    }
-    
-}
+  try {
+    return notesJSON ? JSON.parse(notesJSON) : [];
+  } catch (e) {
+    return [];
+  }
+};
 
 // Create a note
-const createNote = () =>{
-    const id = uuidv4()
-    const timeStamp = moment().valueOf()
-    const note = {
-        id,
-        title: '',
-        body: '',
-        createdAt: timeStamp,
-        updatedAt: timeStamp
-    }
-    notes.push ( note )
-    saveNotes()
+const createNote = () => {
+  const id = uuidv4();
+  const timeStamp = moment().valueOf();
+  const note = {
+    id,
+    title: '',
+    body: '',
+    createdAt: timeStamp,
+    updatedAt: timeStamp,
+  };
+  notes.push(note);
+  saveNotes();
 
-    return id
-}
+  return id;
+};
 
-// Save notes 
+// Save notes
 const saveNotes = () => {
-    const notesJSON = JSON.stringify ( notes )
-    localStorage.setItem ( 'notes', notesJSON )
-}
+  const notesJSON = JSON.stringify(notes);
+  localStorage.setItem('notes', notesJSON);
+};
 
 // remove a note from the list
-const removeNote = ( noteId ) => {
-    const index = notes.findIndex ( ( note ) => note.id === noteId )
-    if ( index > -1 ) {
-        notes.splice ( index, 1 )
-        saveNotes()
-    }    
-}
+const removeNote = (noteId) => {
+  const index = notes.findIndex((note) => note.id === noteId);
+  if (index > -1) {
+    notes.splice(index, 1);
+    saveNotes();
+  }
+};
 
 // Sort notes by one of the selected ways
-const sortNotes = ( sortBy ) => {
+const sortNotes = (sortBy) => {
+  if (sortBy === 'byEdited') {
+    return notes.sort((a, b) => {
+      if (a.updatedAt < b.updatedAt) {
+        return 1;
+      } else if (a.updatedAt > b.updatedAt) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === 'byCreated') {
+    return notes.sort((a, b) => {
+      if (a.createdAt < b.createdAt) {
+        return 1;
+      } else if (a.createdAt > b.createdAt) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  } else if ((sortBy = 'alphabetical')) {
+    return notes.sort((a, b) => {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else {
+    return notes;
+  }
+};
 
-    if ( sortBy === 'byEdited' ) {
-        return notes.sort ( ( a, b ) => {
-            if ( a.updatedAt < b.updatedAt ) {
-                return 1
-            } else if ( a.updatedAt > b.updatedAt ) {
-                return -1
-            } else {
-                return 0
-            }
-        } )
-    } else if ( sortBy === 'byCreated' ) {
-        return notes.sort ( ( a, b ) => {
-            if ( a.createdAt < b.createdAt ) {
-                return 1
-            } else if ( a.createdAt > b.createdAt ) {
-                return -1
-            } else {
-                return 0
-            }
-        } )
-    } else if ( sortBy = 'alphabetical' ) {
-        return notes.sort ( ( a, b ) => {
-            if ( a.title.toLowerCase() < b.title.toLowerCase() ) {
-                return -1
-            } else if ( b.title.toLowerCase() < a.title.toLowerCase() ) {
-                return 1
-            } else {
-                return 0
-            }
-        } )
-    } else {
-        return notes
-    }
-    
-}
+const updateNote = (noteId, updates) => {
+  const note = notes.find((note) => note.id === noteId);
 
-const updateNote = ( noteId, updates ) => {
+  if (!note) {
+    return;
+  }
 
-    const note = notes.find ( ( note ) => note.id === noteId  )
+  if (typeof updates.title === 'string') {
+    note.title = updates.title;
+    note.updatedAt = moment().valueOf();
+  }
 
-    if ( !note ) {
-        return
-    }
+  if (typeof updates.body === 'string') {
+    note.body = updates.body;
+    note.updatedAt = moment().valueOf();
+  }
 
-    if ( typeof updates.title === 'string' )  {
-        note.title = updates.title
-        note.updatedAt = moment().valueOf()
-    }
+  saveNotes();
 
-    if ( typeof updates.body === 'string' ) {
-        note.body = updates.body
-        note.updatedAt = moment().valueOf()
-    }
+  return note.updatedAt;
+};
 
-    saveNotes ()
-
-    return note.updatedAt
-
-}
-
-notes = loadNotes()
+notes = loadNotes();
 
 // get saved notes
-const getNotes = () => notes
+const getNotes = () => notes;
 
-export { getNotes, createNote, removeNote, sortNotes, updateNote }
+export { getNotes, createNote, removeNote, sortNotes, updateNote };
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require ( 'path' )
+const path = require('path');
 
 module.exports = {
-
-    entry: {
-        index: [ 'babel-polyfill', './src/index.js' ],
-        edit: [ 'babel-polyfill', './src/edit.js' ]
+  entry: {
+    index: ['core-js/stable', './src/index.js'],
+    edit: ['core-js/stable', './src/edit.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/scripts'),
+    filename: '[name]-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
     },
-    output: {
-        path: path.resolve ( __dirname, 'public/scripts' ),
-        filename: '[name]-bundle.js'
+    devMiddleware: {
+      publicPath: '/scripts/',
     },
-
-    // babel configuratiom
-    module: {
-        rules: [ {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [ 'env' ]
-                }
-            }
-        } ]
-    },
-
-    // webpack-dev-server configuration
-    devServer: {
-        contentBase: path.resolve ( __dirname, 'public' ),
-        publicPath: '/scripts/'
-    },
-
-    // configure source map
-    devtool: 'source-map'
-
-}
+    open: true, // To open browser automatically after running code
+    // hot: 'only',
+  },
+  devtool: 'source-map',
+};
